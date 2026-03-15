d7c2e2602ea102451ebe0508b119cffb26c660b4
Pekarski <kiryl.pekarski.dev@gmail.com>
2026-03-16 02:04:32 +0300

    feat(payload-plugin-comments): add overlay to CommentsDrawer

--- packages/payload-plugin-comments/src/components/CommentsDrawer/index.tsx
+++ packages/payload-plugin-comments/src/components/CommentsDrawer/index.tsx
@@ -1,21 +1,33 @@
 "use client";
 
+import { createPortal } from "react-dom";
 import { CommentsPanel } from "../CommentsPanel";
 import { Header } from "./components/Header";
 import { cn } from "../../utils/general/cn";
-import { Drawer } from "@payloadcms/ui";
+import { Drawer, useModal } from "@payloadcms/ui";
 
 interface Props {
   slug: string;
 }
 
 export function CommentsDrawer({ slug }: Props) {
+  const { modalState, closeModal, containerRef } = useModal();
+  const isOpen = !!modalState[slug]?.isOpen;
+
   return (
-    <Drawer
-      className={cn("comments-drawer max-w-150 w-full m-0 ml-auto relative")}
-      slug={slug}
-      Header={<Header slug={slug} />}>
-      <CommentsPanel className="pb-5" />
-    </Drawer>
+    <>
+      {isOpen
+        && containerRef.current
+        && createPortal(
+          <div className="fixed inset-0 bg-black/50 z-29 cursor-pointer" onClick={() => closeModal(slug)} />,
+          containerRef.current,
+        )}
+      <Drawer
+        className={cn("comments-drawer max-w-150 w-full m-0 ml-auto relative")}
+        slug={slug}
+        Header={<Header slug={slug} />}>
+        <CommentsPanel className="pb-5" />
+      </Drawer>
+    </>
   );
 }
